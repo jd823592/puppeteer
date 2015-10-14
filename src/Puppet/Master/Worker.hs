cc64e1aa40e84e6c09b882dc69b7013ff53b9629
Jakub Daniel <jakub.daniel@gmail.com>
2015-10-14 17:57:03 +0200

    worker

---
+++ src/Puppet/Master/Worker.hs
@@ -0,0 +1,21 @@
+module Puppet.Master.Worker where
+
+import Control.Concurrent
+import Control.Monad
+import Control.Monad.IO.Class
+
+newtype Worker m = Worker { requests :: Chan (m ()) }
+
+newWorker :: MonadIO m => (m () -> IO ()) -> IO (Worker m)
+newWorker unlift = do
+    channel <- newChan
+    let loop = forever . join . liftIO $ readChan channel
+    forkIO $ unlift loop
+    return $ Worker channel
+
+ask :: MonadIO m => Worker m -> m a -> IO a
+ask w a = do
+    ref <- newEmptyMVar
+    writeChan (requests w) $ do
+        liftIO . putMVar ref =<< a
+    takeMVar ref
